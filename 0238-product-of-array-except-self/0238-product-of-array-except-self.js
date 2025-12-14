86a87437e732fa6fd14c376da7fb15e609b176a7
Wladyslaw Rouba <roubovlad@gmail.com>
2025-12-14 21:43:35 +0100

    Time: 2 ms (96.54%), Space: 73.2 MB (27.05%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    const answer = new Array(nums.length);
+    let leftProduct = 1;
+    for(let i = 0; i < nums.length; i++){
+        answer[i] = leftProduct;
+        leftProduct *= nums[i];
+    }
+      let rightProduct = 1;
+      for(let i = nums.length -1; i >= 0; i--){
+        answer[i] *= rightProduct;
+        rightProduct *= nums[i];
+      }
+      return answer
+};
