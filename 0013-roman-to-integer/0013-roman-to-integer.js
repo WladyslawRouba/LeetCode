1ce4afdd8c705587df0adb26c993295b55c117f6
Wladyslaw Rouba <roubovlad@gmail.com>
2026-04-27 00:32:50 +0200

    Time: 3 ms (91.38%), Space: 61.2 MB (48.75%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    let result = 0
+    let map = {
+I:1,
+V:5,
+X:10,
+L:50,
+C:100,
+D:500,
+M:1000
+    }
+    for(let i = 0; i < s.length; i++){
+       const current = map[s[i]]
+       const next = map[s[i + 1]]
+       if(current < next){
+       result -= current
+       }else{
+        result += current
+       }
+    }
+    return result
+};
