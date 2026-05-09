eeb5bad7cf73fed20a468e8ff68554e889123fc8
Wladyslaw Rouba <roubovlad@gmail.com>
2026-05-09 11:56:57 +0200

    Time: 9 ms (84.94%), Space: 57.3 MB (55.47%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,5 +4,25 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-   return s.split('').sort().join('') === t.split('').sort().join('')
+//    return s.split('').sort().join('') === t.split('').sort().join('')
+if(s.length !== t.length){
+    return false
+}
+let letters = {}
+for(let char of s){
+    if(!letters[char]){
+        letters[char] = 1
+    }else{
+       letters[char]++ 
+    }
+}
+for(let char of t){
+    if(!letters[char]){
+        return false
+    }
+
+ letters[char]--
+
+}
+return true
 };
