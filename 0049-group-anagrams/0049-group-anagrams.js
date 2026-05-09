c75c47f6ce230908f5ebb98206946dbe26a873dc
Wladyslaw Rouba <roubovlad@gmail.com>
2026-05-09 12:38:56 +0200

    Time: 26 ms (82.01%), Space: 69.4 MB (17.64%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -4,13 +4,14 @@
  */
 var groupAnagrams = function(strs) {
     const map = new Map()
-    for (let word of strs){
-        const key = word.split('').sort().join('')
-
+    for(let word of strs){
+        let key = word.split('').sort().join('')
         if(!map.has(key)){
-            map.set(key,[])
+            map.set(key, [])
         }
         map.get(key).push(word)
     }
+
     return Array.from(map.values())
+
 };
