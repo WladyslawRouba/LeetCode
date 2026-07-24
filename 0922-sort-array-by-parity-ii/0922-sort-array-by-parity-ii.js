2fa3aaff0a9f62fd6f86b89d9e8870e5e8e8b692
Wladyslaw Rouba <roubovlad@gmail.com>
2026-07-24 20:19:51 +0200

    Time: 8 ms (27.38%), Space: 61 MB (59.13%) - LeetHub

---
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParityII = function(nums) {
+    const result = new Array(nums.length)
+    let eventIndex = 0
+    let oddIndex = 1
+    for(let num of nums){
+        if(num % 2 === 0){
+            result[eventIndex] = num
+            eventIndex += 2
+        }else{
+            result[oddIndex]= num
+            oddIndex += 2
+        }
+       
+    }
+    return result
+};
